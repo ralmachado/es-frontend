14db3d54ad26ea3e5fc3438386cc21481c0cd857
Rodrigo Machado <29443508+ralmachado@users.noreply.github.com>
2023-04-29 21:48:01 +0100

    React with Vite project init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # es-frontend
 Pharmacy frontend for Service Engineering
+
+Using Vite + React (Typescript)
---
+++ src/main.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
+import Home from './routes/Home.tsx'
+import Login from './routes/Login.tsx'
+import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/login',
+    element: <Login />
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
